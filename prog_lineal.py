6ae0db68e3f495065b6282ce48ac37abc6322779
NestorPala <npalavecino@fi.uba.ar>
2024-06-17 20:54:50 -0300

    improved LP solution output

--- prog_lineal.py
+++ prog_lineal.py
@@ -1,15 +1,52 @@
 import pulp
 
 
+GUERRERO_NAME = 0
+GUERRERO_VALUE = 1
+
+KEY_SEPARATOR = "_"
+KEY_GROUP = 1
+KEY_NAME = 2
+
+
 def key(j, i):
-    return f"a{j}{i}"
+    return f"a{KEY_SEPARATOR}{j}{KEY_SEPARATOR}{i}"
+
+
+def get_boolean(value):
+    if int(value) == 0:
+        return False
+    return True
+
+
+def get_group_number(key):
+    return int(key.split(KEY_SEPARATOR)[KEY_GROUP])
+
+
+def get_name(key, lista_guerreros):
+    index = int(key.split(KEY_SEPARATOR)[KEY_NAME]) - 1
+    return lista_guerreros[index][GUERRERO_NAME]
+
+
+def to_name_in_group(variable_item, pulp, lista_guerreros):
+    variable_name, variable = variable_item
+
+    is_in_group = get_boolean(pulp.value(variable))
+
+    if not is_in_group:
+        return None
+    
+    return [
+        get_group_number(variable_name),
+        get_name(variable_name, lista_guerreros)
+    ]
 
 
 def tribu_agua_ple(lista_guerreros, k):
     problem = pulp.LpProblem("tribu_agua", pulp.LpMinimize)
 
     # coefficients
-    constants = list(map(lambda guerrero: guerrero[1], lista_guerreros))
+    constants = list(map(lambda guerrero: guerrero[GUERRERO_VALUE], lista_guerreros))
 
     # binary variables to solve
     n = len(lista_guerreros)
@@ -51,9 +88,15 @@ def tribu_agua_ple(lista_guerreros, k):
     print("Solving integer lineal programming problem...")
 
     problem.solve()
-    result = list(map(lambda variable: [variable, pulp.value(variable)], variables.values()))
+    
+    # DEBUG
+    # print("------------------------- DEBUG ------------------------- ")
+    # solved_variables = list(map(lambda variable: [get_name(variable[0], lista_guerreros), variable[0], pulp.value(variable[1])], variables.items()))
+    # for v in solved_variables:
+    #     print(v)
+    # print("--------------------------------------------------------- ")
 
-    return problem, result
+    return problem, variables
 
 
 def main():
@@ -67,14 +110,15 @@ def main():
 
     k = 2
 
-    problem, result = tribu_agua_ple(lista_guerreros, k)
+    problem, variables = tribu_agua_ple(lista_guerreros, k)
 
-    # debug
-    print(
-        50 * "--" + "\nPROBLEM: \n", 
-        problem, 
-        "\n" + 50 * "--"
-    )
+    # DEBUG
+    # print("------------------------- DEBUG ------------------------- ")
+    # print(problem)
+    # print("--------------------------------------------------------- ")
+
+    result = list(map(lambda variable: to_name_in_group(variable, pulp, lista_guerreros), variables.items()))
+    result = list(filter(lambda item: item is not None, result))
 
     print("Solution: \n")
     for variable in result:
