d53be70202fee888dad4915197f97c7f0dd4e3ad
NestorPala <npalavecino@fi.uba.ar>
2024-06-24 05:00:52 -0300

    modified tp3.py

--- tp3.py
+++ tp3.py
@@ -1,12 +1,32 @@
 from datetime import datetime
+import os
 import sys
 import time
 from ej003_backtracking import tribu_agua_backtracking
 from ej004_programacion_lineal import tribu_agua_lp
 from ej005_algoritmo_de_aproximacion import tribu_agua_aproximacion
 from ej006_greedy import tribu_agua_greedy
-from utils.tribu_agua import format_result, read_guerreros_file
+from utils.tribu_agua import format_result
 
+
+ARG_COUNT = 4
+
+FILE_OPTION = "-file"
+ABS_OPTION = "-abs"
+
+USO = f"""
+    Uso del programa:
+
+    El archivo tiene que estar en la misma carpeta que tp3.py:
+
+        python file.py {FILE_OPTION} ARCHIVO_GUERREROS.txt SOLVER
+
+    El archivo puede estar en cualquier ruta:
+
+        python file.py {ABS_OPTION} PATH_TO_/ARCHIVO_GUERREROS.txt SOLVER
+
+    SOLVER : "-back", "-lp", "-aprox" o "-greedy"
+"""
 
 SOLVERS = {
     "-back":     lambda guerreros, k: tribu_agua_backtracking(guerreros, k),
@@ -17,6 +37,10 @@ SOLVERS = {
 
 
 def read_file(path):
+    pass
+
+
+def write_file(path):
     pass
 
 
@@ -43,7 +67,34 @@ def solve(f, path):
 
     print(f"File {name} processed!\n")
 
+    return result, coefficient
+
+
+def main():
+    if len(sys.argv) != ARG_COUNT:
+        print(USO)
+        return
+    
+    arg_filemode = sys.argv[1]
+    arg_path     = sys.argv[2]
+    arg_solver   = sys.argv[3]
+    
+    if (arg_filemode == FILE_OPTION):
+        path = arg_path
+    elif(arg_filemode == ABS_OPTION):
+        path = os.path.normpath(arg_path)
+    else:
+        print(USO)
+        return
+    
+    result, coefficient = solve(SOLVERS[arg_solver], path)
+
+    write_file(path, result, coefficient)
+
+    filename = os.path.basename(path)
+    print("\nArchivo procesado con éxito!")
+    print(f"Los resultados se encuentran en el archivo solved_{filename}")
+
 
 if __name__ == "__main__":
-    method = sys.argv[1]
-    solve(SOLVERS[method], "")
+    main()
